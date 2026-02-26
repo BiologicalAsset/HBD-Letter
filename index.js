8e5b018460a3fb8b96e20f3c6e5dc311ed1bf962
BiologicalAsset <ariefakbar1999@gmail.com>
2026-02-26 23:27:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const body = document.body;
 const left = document.getElementById("leftSide");
 const right = document.getElementById("rightSide");
 
-left.addEventListener("mouseenter", () => {
-    body.classList.remove("default");
-    body.classList.add("mint");
-});
-
-right.addEventListener("mouseenter", () => {
-    body.classList.remove("mint");
-    body.classList.add("default");
-});
 
 left.addEventListener("click", () => {
     window.location.href = "Ajeng.html";
